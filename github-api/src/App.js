b59f77b90166359722291ce4f06857b7bee992e2
Tamara Trefilova <tamara.trefilova@gmail.com>
2021-05-16 14:05:34 -0700

    new commit

---
+++ github-api/src/App.js
@@ -0,0 +1,42 @@
+
+import './App.css';
+import GithubUserSearch from './GithubUserSearch';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import GithubUser from './GitHubUser'
+
+function NavBar() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li><Link to="/github/search">GitHub search</Link></li>
+      <li><Link to="/github/users/tamaratrefilova">Tamara's GitHub</Link></li>
+      <li><Link to="/github/users/kbnewlon">Kayla's GitHub</Link></li>
+    </ul>
+
+  )
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path='/' >
+            <h1>Home</h1>
+          </Route>
+          <Route exact path='/github/search' component ={GithubUserSearch} />
+          <Route path='/github/users/:username' component={GithubUser}  />                   
+        </Switch>
+
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ github-api/src/GitHubUser.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+
+class GithubUser extends Component {
+    state = {
+        isLoading: true,
+        githubUser: null,
+        error: false
+    }
+
+    // componentDidMount() {
+
+    //     console.log("from user search", this.props);
+    //     const username = this.props.match.params.username;
+    //     console.log("username", username);
+    //     fetch(`https://api.github.com/users/${username}`)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data);
+    //             this.setState({
+    //                 isLoading: false,
+    //                 githubUser: data
+    //             })
+    //         })
+    //         .catch(()=> {
+    //             this.setState({
+    //                 isLoading:false,
+    //                 error: true
+    //             })
+    //         })
+    // }
+
+    componentDidMount() {
+        this.userSearch();
+
+    }
+
+componentDidUpdate(prevProps){
+    const oldUserName =  prevProps.match.params.username;
+    const incomingUserName = this.props.match.params.username;
+    console.log(oldUserName, incomingUserName);
+    if(oldUserName !== incomingUserName){
+        this.userSearch();
+    }
+}
+
+    userSearch = () => {
+
+        console.log("from user search", this.props);
+        const username = this.props.match.params.username;
+        console.log("username", username);
+        fetch(`https://api.github.com/users/${username}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    isLoading: false,
+                    githubUser: data
+                })
+            })
+            .catch(() => {
+                this.setState({
+                    isLoading: false,
+                    error: true
+                })
+            })
+    }
+
+
+    render() {
+
+        const { isLoading, githubUser, error } = this.state;
+        let content;
+
+        if (githubUser) {
+            content = (
+                <div>
+                    <h2>{githubUser.name}</h2>
+                    <img
+                        style={{ width: '100px' }}
+                        src={githubUser.avatar_url}
+                        alt={`${githubUser.name}'s avatar`}
+                    />
+
+                </div>
+            )
+        }
+        return (
+            <div>
+
+                <h1>GitHub User</h1>
+                {isLoading && <p>Loading...</p>}
+                {error && <p>Error...Please refresh and try again</p>}
+                {content}
+            </div>
+        )
+
+    }
+
+}
+
+export default GithubUser;
